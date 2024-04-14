d7bdfe7320968712312748ea05518e2766602363
Luyolo23 <luyolo.tuta23@gmail.com>
2024-04-14 13:42:41 -0700

    a script that lists all City objects from the database hbtn_0e_101_usa

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python3
-from relationship_state import Base, State
-from relationship_city import City
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy import create_engine
-import sys
+
 """
-    Module that performs MySQL query through MySQLAlchemy.
+Script that lists all City objects from the database hbtn_0e_101_usa.
 """
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_city import Base, City
+from sys import argv
 
 if __name__ == "__main__":
-    db_uri = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
-                                                            sys.argv[1],
-                                                            sys.argv[2],
-                                                            sys.argv[3])
+    # Check if the number of arguments is correct
+    if len(argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(argv[0]))
+        exit()
 
-    engine = create_engine(db_uri, pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    # Creating the connection string
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
+    # Creating an instance of Session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for a_city in session.query(City).order_by(City.id):
-            print("{}: {} -> {}".format(a_city.id, a_city.name,
-                  a_city.state.name))
+    # Querying all City objects with their corresponding State objects
+    # using the state relationship
+    cities = session.query(City).order_by(City.id).all()
 
+    # Displaying the results
+    for city in cities:
+        state_name = city.state.name if city.state else "None"
+        print("{}: {} -> {}".format(city.id, city.name, state_name))
+
+    # Closing the session
     session.close()
